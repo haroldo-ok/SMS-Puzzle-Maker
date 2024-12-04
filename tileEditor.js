fde9718aaa4cb8ed503717b49a185bf03d573833
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-12-04 18:33:46 -0300

    Bump version to `0.10.0`

--- tileEditor.js
+++ tileEditor.js
@@ -378,7 +378,7 @@ var tinyMapEditor = (function() {
 			const project = {
 				tool: {
 					name: APP_NAME,
-					version: '0.9.0',
+					version: '0.10.0',
 					format: '0.1.0'
 				},
 				options: {
