b0bdf3a3f62c838a3806bdbd4e654e70042e49eb
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2025-01-29 21:17:24 -0300

    Create DOM utility function.

---
+++ dom-util.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(() => {
+	/** Adapted from https://kyleshevlin.com/how-to-write-your-own-javascript-dom-element-factory/ */
+	const elFactory = (type, attributes, ...children) => {
+		const el = document.createElement(type);
+
+		for (key in attributes) {
+			el.setAttribute(key, attributes[key]);
+		}
+
+		children.forEach(child => {
+			if (typeof child === 'string') {
+				el.appendChild(document.createTextNode(child))
+			} else {
+				el.appendChild(child)
+			}
+		});
+
+		return el
+	};
+	
+	window.h = elFactory;
+})();
