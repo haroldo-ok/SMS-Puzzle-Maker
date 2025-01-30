e2b02fc2b4238db410e878597c21a41b288d47b5
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2025-01-29 22:12:09 -0300

    Fix data binding for checkbox

--- tileEditor.js
+++ tileEditor.js
@@ -470,15 +470,18 @@ var tinyMapEditor = (function() {
 			const createDiv = (...children) => h('div', {}, ...children);
 			const createCheckbox = attributes => h('input', { type: 'checkbox', ...attributes });
 			
-			const createDataCheckbox = (object, attrName, ...attributes) => createCheckbox({
-				checked: object[attrName],
-				onclick: e => {
+			const createDataCheckbox = (object, attrName, attributes) => {
+				const checkbox = createCheckbox(attributes || {});
+				
+				checkbox.checked = object[attrName];
+				checkbox.addEventListener('click', e => {
 					const target = e.target || e.srcElement;					
 					object[attrName] = target.checked;
-					console.log('Clicked on checkbox', { checked: target.checked, tileAttrs });
-				},
-				...attributes
-			});
+					console.log('Clicked on checkbox', { checked: target.checked, tileAttrs });					
+				});
+				
+				return checkbox;
+			}
 			
 			const headerRow = ['#', 'Tile', 'Solid?', 'Player Start?', 'Player End?']
 				.map(name => h('th', {}, name));				
